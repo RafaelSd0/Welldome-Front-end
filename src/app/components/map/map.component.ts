3f946ce45fd2d902410df6102a4525a2f387fff0
RafaelSd0 <player08901@gmail.com>
2024-10-26 19:48:43 -0300

    adiciona servive no mapa

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -5,6 +5,7 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import { fromLonLat } from 'ol/proj';
+import { GeolocalizacaoService } from '../../services/geolocalizacao.service';
 
 @Component({
   selector: 'app-map',
@@ -13,26 +14,37 @@ import { fromLonLat } from 'ol/proj';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
   map!: Map;
+  constructor(private geolocalizacao: GeolocalizacaoService) {}
 
   ngOnInit(): void {
-    this.inicializarMapa();
+    this.geolocalizacao.obterLocalizacao().then((cords) => {
+      this.inicializarMapa(cords.latitude, cords.longitude);
+    }).catch((error) => {
+      console.error('Erro ao obter localização:', error);
+      // Defina coordenadas padrão caso a localização falhe
+      this.inicializarMapa(-23.533773, -46.62529); // São Paulo, por exemplo
+    });
+
   }
 
-  inicializarMapa(): void {
-  this.map = new Map({
-    target: 'map', // Elemento div onde o mapa será renderizado
-    layers: [
-      new TileLayer({
-        source: new OSM() // Usando OpenStreetMap como camada de fundo
-      })
-    ],
-    view: new View({
-      center: fromLonLat([-46.62529, -23.533773]), // Coordenadas iniciais (São Paulo)
-      zoom: 12 // Nível de zoom inicial
-    })
-  });
-}
 
+  inicializarMapa(latitude: number, longitude: number): void {
+    // Converte latitude e longitude para a projeção do OpenLayers
+    const coordenadas = fromLonLat([longitude, latitude]);
+
+    this.map = new Map({
+      target: 'map', // Elemento onde o mapa será renderizado
+      layers: [
+        new TileLayer({
+          source: new OSM() // Usando OpenStreetMap como camada de fundo
+        })
+      ],
+      view: new View({
+        center: coordenadas, // Coordenadas convertidas para projeção correta
+        zoom: 12 // Nível de zoom inicial
+      })
+    });
+  }
 }
