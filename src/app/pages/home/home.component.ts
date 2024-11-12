f89cc4610bb6c6d28bc854f1a45633ab8de02ad4
RafaelSd0 <player08901@gmail.com>
2024-11-12 17:42:59 -0300

    delet map

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { MapComponent } from '../../components/map/map.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MapComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
