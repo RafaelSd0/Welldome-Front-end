b6659b1135e4ebaf5534069bfab47fed08bad58f
RafaelSd0 <player08901@gmail.com>
2024-11-22 23:30:25 -0300

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotificationPageComponent } from './pages/notification-page/notification-page.component';
+import { GraphsComponent } from './pages/graphs/graphs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'inicio',
+    component: HomeComponent,
+  },
+  {
+    path: 'notificacoes',
+    component: NotificationPageComponent,
+  },
+  {
+    path: 'graficos',
+    component: GraphsComponent,
+  }
+];
