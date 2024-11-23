e7cbb7e9349d7c83514ef1eac817aa63613fa590
RafaelSd0 <player08901@gmail.com>
2024-11-22 23:30:18 -0300

    app modifications pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
-import { GraphsComponent } from "./pages/graphs/graphs.component";
 import { FooterComponent } from "./components/footer/footer.component";
-import { NotificationPageComponent } from "./pages/notification-page/notification-page.component";
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent, GraphsComponent, FooterComponent, NotificationPageComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
