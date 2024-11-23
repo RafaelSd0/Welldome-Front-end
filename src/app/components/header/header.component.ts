2b4c454c0ca85d8efa9ac19f952bbff4597c7271
RafaelSd0 <player08901@gmail.com>
2024-11-22 23:30:39 -0300

    header modifications

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { RouterOutlet, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, RouterOutlet, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
