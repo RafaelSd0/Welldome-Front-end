a71f673b58c1bced9d421f683412f680ecf06a37
RafaelSd0 <player08901@gmail.com>
2024-11-22 22:18:37 -0300

    modificações no app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { GraphsComponent } from "./pages/graphs/graphs.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NotificationPageComponent } from "./pages/notification-page/notification-page.component";
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, GraphsComponent, FooterComponent, NotificationPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Welldome';
+
 }
